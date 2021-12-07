83c8b1f735e4b91257368efaebe9edde0451cf7f
Farangiz0102 <hamroalievaf@gmail.com>
2021-12-07 16:23:40 +0300

    first commit

---
+++ views/site/about.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'Дар бораи мо';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1>Hello World! This is About page!</h1>
+</div>
---
+++ views/site/index.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron text-center bg-transparent">
+        <h1 class="display-4">Хуш омадед ба сомонаи ҶДММ Фаровон-1!</h1>
+    </div>
+</div>
